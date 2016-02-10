22f778df26f39b34fcf9dccd76990ccfa0f60de6
Thom Bruce <thom@thombruce.com>
2016-02-10 09:28:57 +0000

    basic angular not really setup

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,8 @@
 // require jquery.turbolinks
 //= require jquery_ujs
 //
+//= require fastclick
+//
 //= require foundation-apps
 //= require_tree ../../../vendor/assets/javascripts/services/.
 //= require_tree ../../../vendor/assets/javascripts/components/.
@@ -11,6 +13,32 @@
 //Turbolinks.enableProgressBar();
 
 //angular.module('modalMarmalade', ['ui.router','ngAnimate','foundation','foundation.dynamicRouting','foundation.dynamicRouting.animations']);
+
+(function() {
+  'use strict';
+
+  angular.module('application', ['ui.router','ngAnimate','foundation','foundation.dynamicRouting','foundation.dynamicRouting.animations']).config(config).run(run);
+
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function config($urlProvider, $locationProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled: false,
+      requireBase: false
+    });
+
+    $locationProvider.hashPrefix('!');
+  }
+  
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+    
+})();
+
 
 $(document).ready(function(){
   //angular.bootstrap($("#modalMarmalade"), ['modalMarmalade'])
