b4176d67d0be613d67764a3a4b6cba6539738e03
Thom Bruce <thom@thombruce.com>
2016-02-14 11:10:38 +0000

    probable collaborator fix

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,5 +1,4 @@
 class Collaborator < ActiveRecord::Base
-  # TODO: May have fucked up the migration. Check and correct.
   belongs_to :subscriber, :class_name => "User", counter_cache: true
-  belongs_to :collaborator, :class_name => "User", counter_cache: true
+  belongs_to :collaborator, :class_name => "User", :foreign_key => 'collaborator_id', counter_cache: true
 end
