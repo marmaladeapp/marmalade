a1e988760b547bc2f0ca7b141df5a06dbb2a546b
Thom Bruce <thom@thombruce.com>
2016-02-27 16:53:46 +0000

    controllify payments

---
+++ app/controllers/app/finances/payments_controller.rb
@@ -0,0 +1,29 @@
+class App::Finances::PaymentsController < App::AppController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def payment_params
+  end
+
+end
