3e39baf10aabfc9df3471077ef9be40f07dc14bb
Thom Bruce <thom@thombruce.com>
2016-04-04 04:56:36 +0100

    add mailer config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,31 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'marmaladeapp.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: ENV["GMAIL_DOMAIN"],
+  #  authentication: "plain",
+  #  enable_starttls_auto: true,
+  #  user_name: ENV["GMAIL_USERNAME"],
+  #  password: ENV["GMAIL_PASSWORD"]
+  #}
+
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    port: 587,
+    domain: ENV["DOMAIN"],
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"]
+  }
 end
